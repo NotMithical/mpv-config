363a2c8cf68a66c1fede2f90613446d9401ddbdd
NotMithical <95518825+NotMithical@users.noreply.github.com>
2023-11-15 23:58:32 -0500

    Personal: Update runYT.sh
    Update format string

--- Personal/runYT.sh
+++ Personal/runYT.sh
@@ -2,4 +2,4 @@
 cd g:\\cygwin\\bin
 OUTPUT=$(./getclip.exe)
 cd c:\\users\\unsha\\mpvstable
-./yt-dlp.exe --hls-use-mpegts --http-chunk-size 10M --concurrent-fragments 2 --downloader "aria2c" --downloader-args aria2c:max-connection-per-server=5 --downloader-args aria2c:min-split-size=10M -f "bestvideo*[vcodec!*=av01][dynamic_range!*=SDR][vbr<40000] / bestvideo*[vcodec!*=avc][vbr<40000] / bestvideo*[vbr<40000] / best" -S "hdr,res,vbr,codec" -o - ${OUTPUT} | ./mpv.exe - --audio-file=$(./yt-dlp.exe -f "ba" --get-url "${OUTPUT}") --force-media-title="$(./yt-dlp.exe --get-title "${OUTPUT}")"
+./yt-dlp.exe --hls-use-mpegts --http-chunk-size 10M --concurrent-fragments 2 --downloader "aria2c" --downloader-args aria2c:max-connection-per-server=5 --downloader-args aria2c:min-split-size=10M -f "bestvideo*[vbr<34000][dynamic_range!*=SDR] / bestvideo*[vbr<34000][vcodec!*=avc] / bestvideo / best" -o - ${OUTPUT} | ./mpv.exe - --audio-file=$(./yt-dlp.exe -f "ba" --get-url "${OUTPUT}") --force-media-title="$(./yt-dlp.exe --get-title "${OUTPUT}")"
