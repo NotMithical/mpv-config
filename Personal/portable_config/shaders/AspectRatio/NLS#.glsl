b1ef4bf83af1d05dd06b4f0ec4630f9c2c650cf7
NotMithical <95518825+NotMithical@users.noreply.github.com>
2024-11-14 22:12:55 -0500

    Update NLS#.glsl
    Lots of cleanup. There was code in here that genuinely did nothing.

--- Personal/portable_config/shaders/AspectRatio/NLS#.glsl
+++ Personal/portable_config/shaders/AspectRatio/NLS#.glsl
@@ -39,38 +39,26 @@
 //!BIND HOOKED
 //!DESC Bidirectional Nonlinear Stretch
 
-vec2 stretch(vec2 pos, float h_par, float v_par)
-{
-	float BarsAmountNorm = BarsAmount * 2.5;
-	float HorizontalStretchNorm = HorizontalStretch * (1 / (HorizontalStretch + VerticalStretch));
-	float VerticalStretchNorm = VerticalStretch * (1 / (HorizontalStretch + VerticalStretch));
+vec2 stretch(vec2 pos, float h_par, float v_par) {
+	// Normalize user defined parameters
+	float BarsAmountNorm = (BarsAmount * 2.5),
+		  HorizontalStretchNorm = (HorizontalStretch * (1 / (HorizontalStretch + VerticalStretch))),
+		  VerticalStretchNorm = (VerticalStretch * (1 / (HorizontalStretch + VerticalStretch)));
 
 	float h_m_stretch = pow(h_par, HorizontalStretchNorm),
 		  v_m_stretch = pow(v_par, VerticalStretchNorm),
 		  x = pos.x - 0.5,
 		  y = pos.y - 0.5;
-		  
-	// Check how far each pixel is past the target boundaries
-    float x_offset = abs(x) - 0.5 + 0.5 / HOOKED_size.x;
-    float y_offset = abs(y) - 0.5 + 0.5 / HOOKED_size.y;
-	
-	// Check if each pixel is outside the target boundaries
-    bool outOfBounds = x_offset > 0.5 || y_offset > 0.5;
-	
-	//Map x & y coordinates to themselves with a curve, taking into account cropping and padding
-	if (h_par < 1)
-	{		
+
+	// Map x & y coordinates to themselves with a curve, taking into account cropping and padding
+	if (h_par < 1) {
 		return vec2(mix(x * abs(x) * (2 - (CropAmount * 2)), x, h_m_stretch) + 0.5, mix(y * abs(y) * (2 - (BarsAmountNorm * 2)), y, v_m_stretch) + 0.5);
-	}
-	
-	else
-	{
+	} else {
 		return vec2(mix(x * abs(x) * (2 - (BarsAmountNorm * 2)), x, h_m_stretch) + 0.5, mix(y * abs(y) * (2 - (CropAmount * 2)), y, v_m_stretch) + 0.5);
 	}
 }
 
-vec4 hook()
-{
+vec4 hook() {
 	float dar = target_size.x / target_size.y,
 		  sar = HOOKED_size.x / HOOKED_size.y,
 		  h_par = dar / sar,
@@ -79,31 +67,8 @@ vec4 hook()
 	vec2 stretchedPos = stretch(HOOKED_pos, h_par, v_par);
 	
 	// Check what pixels are outside the target boundaries
-	bool outOfBounds;
-	
-	if (any(lessThan(stretchedPos, vec2(0.0))) || any(greaterThan(stretchedPos, vec2(1.0))))
-	{
-		outOfBounds = true;
-	}
-	
-	else
-	{
-		outOfBounds = false;
-	}
-
+	bool outOfBounds = ((any(lessThan(stretchedPos, vec2(0.0))) || any(greaterThan(stretchedPos, vec2(1.0)))) ? true : false);
 
 	// Black out pixels outside target boundaries
-	vec4 color;
-	
-	if (outOfBounds == true)
-	{
-		color = vec4(0.0);
-	}
-	
-	else
-	{
-		color = HOOKED_tex(stretchedPos);
-	}
-	
-	return color;
+	return (outOfBounds ? vec4(0.0) : HOOKED_tex(stretchedPos));
 }
