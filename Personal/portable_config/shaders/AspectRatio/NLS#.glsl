454cd6e0209cd503281c41b87360be679a02f959
NotMithical <95518825+NotMithical@users.noreply.github.com>
2024-12-04 15:59:54 -0500

    Update NLS#.glsl

--- Personal/portable_config/shaders/AspectRatio/NLS#.glsl
+++ Personal/portable_config/shaders/AspectRatio/NLS#.glsl
@@ -8,6 +8,8 @@
 //		Crop image edges. Raising this value results in loss of content but results in less stretching.
 // BarsAmount:
 //		Scale the image down and add padding in the form of black bars. Raising this value results in less stretching.
+// CenterProtect:
+//		Changes the curve for stretching. Higher values apply more stretching towards the edges of the screen. Currently experimental; I'm still figuring out the math for this whole thing and it turns out the padding, cropping, and center protection affect each other a fair bit.
 //
 // The defaults will distribute stretching across all edges and will not crop or pad the image.
 
@@ -35,16 +37,22 @@
 //!MAXIMUM 1.0
 0.0
 
+//!PARAM CenterProtect
+//!TYPE float
+//!MINIMUM 0.1
+//!MAXIMUM 6.0
+1.0
+
 //!HOOK MAINPRESUB
 //!BIND HOOKED
 //!DESC Bidirectional Nonlinear Stretch
 
 vec2 stretch(vec2 pos, float h_par, float v_par) {
 	// Normalize user defined parameters
-	float BarsAmountNorm = (BarsAmount * 2.5),
-		  HorizontalStretchNorm = (HorizontalStretch * (1 / (HorizontalStretch + VerticalStretch))),
+	float HorizontalStretchNorm = (HorizontalStretch * (1 / (HorizontalStretch + VerticalStretch))),
 		  VerticalStretchNorm = (VerticalStretch * (1 / (HorizontalStretch + VerticalStretch)));
 
+	//float h_m_stretch = pow(h_par, HorizontalStretchNorm),
 	float h_m_stretch = pow(h_par, HorizontalStretchNorm),
 		  v_m_stretch = pow(v_par, VerticalStretchNorm),
 		  x = pos.x - 0.5,
@@ -52,9 +60,9 @@ vec2 stretch(vec2 pos, float h_par, float v_par) {
 
 	// Map x & y coordinates to themselves with a curve, taking into account cropping and padding
 	if (h_par < 1) {
-		return vec2(mix(x * abs(x) * (2 - (CropAmount * 2)), x, h_m_stretch) + 0.5, mix(y * abs(y) * (2 - (BarsAmountNorm * 2)), y, v_m_stretch) + 0.5);
+		return vec2(mix(x * pow(abs(x), CenterProtect) * (pow(2, CenterProtect) - (CropAmount * 2)), x, h_m_stretch) + 0.5, mix(y * pow(abs(y), CenterProtect) * (pow(2, CenterProtect) - (BarsAmount * 5)), y, v_m_stretch) + 0.5);
 	} else {
-		return vec2(mix(x * abs(x) * (2 - (BarsAmountNorm * 2)), x, h_m_stretch) + 0.5, mix(y * abs(y) * (2 - (CropAmount * 2)), y, v_m_stretch) + 0.5);
+		return vec2(mix(x * pow(abs(x), CenterProtect) * (pow(2, CenterProtect) - (BarsAmount * 5)), x, h_m_stretch) + 0.5, mix(y * pow(abs(y), CenterProtect) * (pow(2, CenterProtect) - (CropAmount * 2)), y, v_m_stretch) + 0.5);
 	}
 }
 
